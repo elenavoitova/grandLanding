d6fd300826a70e898065f5bbf3331227fd95d575
Elena Voitova <elena.voitova.lb@gmail.com>
2020-05-24 23:56:43 +0300

    Updates

---
+++ precache-manifest.1c594b3dc5a07dbdf5833966a758e1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f0ef42ea9d4d643a6eb8730aee7cf4",
+    "url": "/grandLanding/index.html"
+  },
+  {
+    "revision": "c4dd89ff1d87086a36b2",
+    "url": "/grandLanding/static/css/main.6563b0e5.chunk.css"
+  },
+  {
+    "revision": "a7196726898424706bb8",
+    "url": "/grandLanding/static/js/2.464ab383.chunk.js"
+  },
+  {
+    "revision": "ee3b17606b797befe8c8130e3d71363b",
+    "url": "/grandLanding/static/js/2.464ab383.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4dd89ff1d87086a36b2",
+    "url": "/grandLanding/static/js/main.66041e4d.chunk.js"
+  },
+  {
+    "revision": "c623baede03586a28dcd",
+    "url": "/grandLanding/static/js/runtime-main.21035fbb.js"
+  }
+]);
