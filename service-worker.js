eb88f2be0fa6fed1f410ce06bb4fb54021159399
Elena Voitova <elena.voitova.lb@gmail.com>
2020-06-26 12:36:57 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grandLanding/precache-manifest.fc87236a01bef902e64cc526f2cbd6c8.js"
+  "/grandLanding/precache-manifest.398edadc44b4b7c44c64a6038afbf96a.js"
 );
 
 self.addEventListener('message', (event) => {
